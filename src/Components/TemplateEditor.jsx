ae25ded1ed7ea7dfb229f11ff47ee94759abd5ed
Lean <leandrohornos@gmail.com>
2021-03-18 22:04:18 -0300

    genero preview simple y guardo templates nuevas

--- src/Components/TemplateEditor.jsx
+++ src/Components/TemplateEditor.jsx
@@ -2,10 +2,21 @@ import React, { useState, useEffect } from "react";
 
 import Button from "react-bootstrap/Button";
 
+// Firebase
+
+import firebaseApp from "../firebaseApp";
+
+// Router
+import { useHistory } from "react-router-dom";
+
 import sectionSchema from "../Models/sectionSchema";
+import templateSchema from "../Models/templateSchema";
 
 const TemplateEditor = () => {
-  const [name, setName] = useState(""); // Nombre de la plantilla
+  const db = firebaseApp.firestore();
+  const history = useHistory();
+
+  const [title, setTitle] = useState(""); // Nombre de la plantilla
   const [sections, setSections] = useState([]);
   const [currentSection, setCurrentSection] = useState(sectionSchema);
   const [sectionIsComplete, setSectionIsComplete] = useState(false);
@@ -24,6 +35,19 @@ const TemplateEditor = () => {
     console.log("se ha agregado una nueva seccion", sections);
   }, [sections]);
 
+  const saveNewTemplate = async () => {
+    let template = { ...templateSchema, title, sections, creatorId: "" };
+
+    console.log("vamos a grabar esta template:", template);
+    try {
+      await db.collection("templates").add(template);
+      console.log("Nueva template creada con exito");
+      history.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <div className="row row-custom-settings">
@@ -34,6 +58,15 @@ const TemplateEditor = () => {
       <div className="row row-custom-settings">
         <div className="col-md-6">
           <div className="center-col-container">
+            <h4>Template Title</h4>
+            <input
+              value={title}
+              type="text"
+              placeholder="Choose a name for the section"
+              onChange={(e) => {
+                setTitle(e.target.value);
+              }}
+            ></input>
             <h4>Add a new section to the template</h4>
             <div>
               <label>Section title</label>
@@ -86,8 +119,24 @@ const TemplateEditor = () => {
         <div className="col-md-6">
           <h4>Template Preview</h4>
           <div className="template-preview-cont">
-            <h4>Hola don pepito</h4>
+            <h5 className="text-center">{title}</h5>
+            {sections.map((section) => {
+              return (
+                <div>
+                  <h5>{section.name}</h5>
+                  <p>{section.component}</p>
+                </div>
+              );
+            })}
           </div>
+          <Button
+            block
+            variant="success"
+            className="block-btn"
+            onClick={saveNewTemplate}
+          >
+            Save Template
+          </Button>
         </div>
       </div>
     </div>
---
+++ src/Models/templateSchema.js
@@ -0,0 +1,10 @@
+const templateSchema = {
+  title: "", // El nombre de la plantilla
+  sections: [], // Las secciones a mostrar, con la data para renderizarlas
+  creatorId: "", // Quien creó la plantilla - Admin
+  creationDate: new Date(), // Cuando fue creada
+  lastModifield: new Date(), // Ultima vez que se guardaron cambios
+  teamId: "", // Para compartir la plantilla con un grupo
+};
+
+export default templateSchema;
